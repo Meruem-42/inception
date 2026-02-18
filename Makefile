b74cacabe04e58bb7d5f2f81f9a1d047e2d86655
Alexandre Herlaud <aherlaud@f4r3s1.clusters.42paris.fr>
2026-02-18 18:28:35 +0100

    new feature (main : new test) : try different method for mariadb script and wordpress. Add env variables to the code. Need to test in a VM

--- Makefile
+++ Makefile
@@ -1,18 +1,29 @@
 COMPOSE = docker compose
 COMPOSE_FILE = ./srcs/docker-compose.yml
+WP_DATA = /home/aherlaud/data/wordpress
+DB_DATA = /home/aherlaud/data/mariadb
 
 .PHONY: up down build restart logs ps
 
+all : up
+
 up:
-	$(COMPOSE) -f $(COMPOSE_FILE) up
+	@mkdir -p $(WP_DATA)
+	@mkdir -p $(DB_DATA)
+	$(COMPOSE) -f $(COMPOSE_FILE) up --build -d
 
 down:
 	$(COMPOSE) -f $(COMPOSE_FILE) down
 
-build:
-	$(COMPOSE) -f $(COMPOSE_FILE) build
+clean : down
+	docker rm -f wordpress mariadb nginx || true
+	docker rmi -f wordpress:user mariadb:user nginx:user || true
+	docker volume rm wordpress mariadb || true
+	docker network rm inception || true
+	rm -rf $(WP_DATA)
+	rm -rf $(DB_DATA)
 
-re: down build up
+re: clean all
 
 logs:
 	$(COMPOSE) -f $(COMPOSE_FILE) logs -f
---
+++ srcs/requirements/wordpress/tools/script.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#---------------------------------------------------wp installation---------------------------------------------------#
+# wp-cli installation
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+# wp-cli permission
+chmod +x wp-cli.phar
+# wp-cli move to bin
+mv wp-cli.phar /usr/local/bin/wp
+
+# go to wordpress directory
+cd /var/www/wordpress
+# give permission to wordpress directory
+chmod -R 755 /var/www/wordpress/
+# change owner of wordpress directory to www-data
+chown -R www-data:www-data /var/www/wordpress
+#---------------------------------------------------wp installation---------------------------------------------------##---------------------------------------------------wp installation---------------------------------------------------#
+
+# download wordpress core files
+wp core download --allow-root
+# create wp-config.php file with database details
+wp core config --dbhost=mariadb:3306 --dbname="$MYSQL_DB" --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --allow-root
+# install wordpress with the given title, admin username, password and email
+wp core install --url="$DOMAIN_NAME" --title="$WP_TITLE" --admin_user="$WP_ADMIN_NAME" --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_ADMIN_EMAIL" --allow-root
+#create a new user with the given username, email, password and role
+wp user create "$WP_USER_NAME" "$WP_USER_EMAIL" --user_pass="$WP_USER_PASSWORD" --allow-root
+#install a theme
+wp theme install twentytwentyone --activate --allow-root
+
+# redis config
+wp plugin install redis-cache --activate --allow-root
+wp config set WP_REDIS_HOST "redis" --allow-root
+wp redis enable --allow-root
+
+
+#---------------------------------------------------php config---------------------------------------------------#
+
+# change listen port from unix socket to 9000
+sed -i '36 s@/run/php/php7.4-fpm.sock@9000@' /etc/php/7.4/fpm/pool.d/www.conf
+# create a directory for php-fpm
+mkdir -p /run/php
+# start php-fpm service in the foreground to keep the container running
+/usr/sbin/php-fpm7.4 -F
